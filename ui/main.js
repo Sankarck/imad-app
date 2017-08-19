0321a0c0878bcab1308d8e46b3eb17a794c10343
Sankarck <sankarck.7@gmail.com>
2017-08-19 15:37:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,10 @@ console.log('Loaded!');
 
 //to move the image
 var img=document.getElementById("click-image");
+function moveRight(){
+    marginRight=marginRight+10;
+    img.style.marginRight=marginRight+"px";
+}
 img.onclick=function(){
-    img.style.marginLeft="100px";
+    var interval=setInterval(moveRight,50);
 };
